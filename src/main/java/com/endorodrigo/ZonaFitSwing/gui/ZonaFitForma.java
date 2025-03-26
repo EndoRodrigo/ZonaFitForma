245a7f33091ef5a73a6c54b2888e0e38c5cf1efc
EndoRodrigoRodriguez <endorodrigo19@gmail.com>
2025-03-25 20:07:45 -0500

    feat: implementacion de la funcionalidad de crear cliente

--- src/main/java/com/endorodrigo/ZonaFitSwing/gui/ZonaFitForma.java
+++ src/main/java/com/endorodrigo/ZonaFitSwing/gui/ZonaFitForma.java
@@ -1,5 +1,6 @@
 package com.endorodrigo.ZonaFitSwing.gui;
 
+import com.endorodrigo.ZonaFitSwing.modelo.Cliente;
 import com.endorodrigo.ZonaFitSwing.servicio.ClienteServicio;
 import com.endorodrigo.ZonaFitSwing.servicio.IClienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @Component
 public class ZonaFitForma extends JFrame {
@@ -26,7 +29,10 @@ public class ZonaFitForma extends JFrame {
     public ZonaFitForma(ClienteServicio clienteServicio){
         this.clientService = clienteServicio;
         iniciarForma();
+        guardarButton.addActionListener(e -> crearCliente());
     }
+
+
 
     private void iniciarForma(){
         setContentPane(panelPrincipal);
@@ -57,5 +63,42 @@ public class ZonaFitForma extends JFrame {
             };
             this.tablaModeloClientes.addRow(renglonCliente);
         });
+    }
+
+    private void crearCliente() {
+        if (nombreTexto.getText().equals("") ) {
+            JOptionPane.showMessageDialog(null,"El campo nombre es obligatorio");
+            nombreTexto.requestFocusInWindow();
+            return;
+        }
+        if (apellidoTexto.getText().equals("")) {
+            JOptionPane.showMessageDialog(null,"El campo apellido es obligatorio");
+            apellidoTexto.requestFocusInWindow();
+            return;
+        }
+        if (membresiaTexto.getText().equals("")) {
+            JOptionPane.showMessageDialog(null,"El campo membresia es obligatorio");
+            membresiaTexto.requestFocusInWindow();
+            return;
+        }
+
+        // Obteniendo los campos del formulario
+        var nombre = nombreTexto.getText();
+        var apellido = apellidoTexto.getText();
+        var membresia = Integer.parseInt(membresiaTexto.getText());
+        var cliente = new Cliente();
+        cliente.setNombre(nombre);
+        cliente.setApellido(apellido);
+        cliente.setMembresia(membresia);
+        this.clientService.guardarCliente(cliente);
+        JOptionPane.showMessageDialog(null,"Cliente guardado correctamente");
+        LimpiarFormulario();
+        listarClientes();
+    }
+
+    private void LimpiarFormulario(){
+        nombreTexto.setText("");
+        apellidoTexto.setText("");
+        membresiaTexto.setText("");
     }
 }
